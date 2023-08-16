b5b20691803deb91cac2240b84d9c6d9143c13d6
Manisha <manisha@dreamtusk.com>
2023-08-16 14:01:20 +0530

    created project

---
+++ ContactsApp/ContactsApp/ContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppApp.swift
+//  ContactsApp
+//
+//  Created by Vicky on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
